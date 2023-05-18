602147e134528e8160b17ca2460bf2a6fb63296c
Uchenna Chibueze <emmachi72.ec@gmail.com>
2023-05-18 22:12:47 +0100

    Added Traffic Manager

--- TFLEARN/backend.tf
+++ TFLEARN/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     storage_account_name = "aisremotebackend"
     container_name       = "tfstate"
-    key                  = "weuterraform.tfstate"  
+    key                  = "eusterraform.tfstate"  
     resource_group_name = "AIS-TFLEARN-RG"
   }
 }
